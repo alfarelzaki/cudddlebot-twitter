b167fb1a869a3fc2fd83c33a2d7eb82285fb5c99
Alfarel Zaki <alfarell.zaki@mail.com>
2020-03-01 20:42:02 +0700

    oke sekarang dah benar

--- cudddlebot.js
+++ cudddlebot.js
@@ -1,6 +1,6 @@
 // define dependencies | mendeklarasikan dependencies
 const twit = require('twit')
-const config = require('../config')
+const config = require('./config')
 var T = new twit(config)
 
 T.get('account/verify_credentials', {
